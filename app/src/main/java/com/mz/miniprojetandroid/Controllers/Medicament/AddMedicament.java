256929dea2a1b3cea58f5d483e42dadd58744511
Abounki Hamza <abounkihamza@gmail.com>
2020-06-30 15:43:19 +0100

    Ajout du Datepicker et de l'icon

--- app/src/main/java/com/mz/miniprojetandroid/Controllers/Medicament/AddMedicament.java
+++ app/src/main/java/com/mz/miniprojetandroid/Controllers/Medicament/AddMedicament.java
@@ -2,13 +2,19 @@ package com.mz.miniprojetandroid.Controllers.Medicament;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mz.miniprojetandroid.Dao.miniProjetDBHelper;
@@ -17,6 +23,7 @@ import com.mz.miniprojetandroid.Models.Medicament;
 import com.mz.miniprojetandroid.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class AddMedicament extends AppCompatActivity {
@@ -25,12 +32,15 @@ public class AddMedicament extends AppCompatActivity {
     private EditText medicamentCategorie;
     private EditText medicamentPrix;
     private EditText medicamentQuantite;
-    private EditText medicamentDate;
+    private EditText mDisplayDate;
     private Button mAddBtn;
     private Spinner spinner;
     private String filter = "";
     int fournisseurId;
+    public String date;
 
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private static final String TAG = "AddMedicament";
 
     private miniProjetDBHelper dbHelper;
 
@@ -43,7 +53,7 @@ public class AddMedicament extends AppCompatActivity {
         medicamentCategorie = (EditText) findViewById(R.id.categorieAdd);
         medicamentPrix = (EditText) findViewById(R.id.prixAdd);
         medicamentQuantite = (EditText) findViewById(R.id.quantiteAdd);
-        medicamentDate = (EditText) findViewById(R.id.dateAdd);
+        mDisplayDate = (EditText) findViewById(R.id.dateAdd);
 
         mAddBtn = (Button) findViewById(R.id.Ajouter);
 
@@ -66,6 +76,35 @@ public class AddMedicament extends AppCompatActivity {
                 new ArrayAdapter<Fournisseur>(getApplicationContext(),android.R.layout.simple_spinner_item,list);
 
         spinner.setAdapter(adapter);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        AddMedicament.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                 date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
     }
 
     private void saveMedicament() {
@@ -73,7 +112,7 @@ public class AddMedicament extends AppCompatActivity {
         String categorie = medicamentCategorie.getText().toString().trim();
         String prix = medicamentPrix.getText().toString().trim();
         String quantite = medicamentQuantite.getText().toString().trim();
-        String date = medicamentDate.getText().toString().trim();
+        String date = this.date;
         Fournisseur four = (Fournisseur) spinner.getSelectedItem();
 
 
--- app/src/main/java/com/mz/miniprojetandroid/Controllers/Medicament/UpdateMedicament.java
+++ app/src/main/java/com/mz/miniprojetandroid/Controllers/Medicament/UpdateMedicament.java
@@ -2,11 +2,16 @@ package com.mz.miniprojetandroid.Controllers.Medicament;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
@@ -20,6 +25,7 @@ import com.mz.miniprojetandroid.Models.Medicament;
 import com.mz.miniprojetandroid.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class UpdateMedicament extends AppCompatActivity {
@@ -28,12 +34,16 @@ public class UpdateMedicament extends AppCompatActivity {
     private EditText medicamentCategorie;
     private EditText medicamentPrix;
     private EditText medicamentQuantite;
-    private EditText medicamentDate;
+    private EditText mDisplayDate;
     private Button mUpdateBtn;
     private Spinner spinner;
     private miniProjetDBHelper dbHelper;
     private long receivedMedicamentId;
     private String filter="";
+    public String date;
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private static final String TAG = "UpdateMedicament";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +54,7 @@ public class UpdateMedicament extends AppCompatActivity {
         medicamentCategorie = (EditText) findViewById(R.id.categorieUpdate);
         medicamentPrix = (EditText) findViewById(R.id.prixUpdate);
         medicamentQuantite = (EditText) findViewById(R.id.quantiteUpdate);
-        medicamentDate = (EditText) findViewById(R.id.dateUpdate);
+        mDisplayDate = (EditText) findViewById(R.id.dateUpdate);
         mUpdateBtn = (Button) findViewById(R.id.modifier);
         spinner = findViewById(R.id.spinnerUpdate);
         dbHelper = new miniProjetDBHelper(UpdateMedicament.this);
@@ -60,7 +70,7 @@ public class UpdateMedicament extends AppCompatActivity {
         medicamentCategorie.setText(queriedMedicament.getCategorie());
         medicamentPrix.setText(queriedMedicament.getPrix());
         medicamentQuantite.setText(queriedMedicament.getQuantite());
-        medicamentDate.setText(queriedMedicament.getDate());
+        mDisplayDate.setText(queriedMedicament.getDate());
         /*Fournisseur f = dbHelper.getFournisseur(queriedMedicament.getFournisseur_id());
         String fr = f.getNom();
         spinner.setPrompt(fr);*/
@@ -71,6 +81,35 @@ public class UpdateMedicament extends AppCompatActivity {
         ArrayAdapter<Fournisseur> adapter = new ArrayAdapter<Fournisseur>(getApplicationContext(),android.R.layout.simple_spinner_item,list);
 
         spinner.setAdapter(adapter);
+
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        UpdateMedicament.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
 
         mUpdateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,7 +125,7 @@ public class UpdateMedicament extends AppCompatActivity {
         String categorie = medicamentCategorie.getText().toString().trim();
         String prix = medicamentPrix.getText().toString().trim();
         String quantite = medicamentQuantite.getText().toString().trim();
-        String date = medicamentDate.getText().toString().trim();
+        String date = mDisplayDate.getText().toString().trim();
         Fournisseur four = (Fournisseur) spinner.getSelectedItem();
         int fournisseurId = four.getId();
         dbHelper = new miniProjetDBHelper(UpdateMedicament.this);
